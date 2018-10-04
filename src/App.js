e2819313043d38b5cd19f5206d60e54949ba8d30
Nikolay Myslovskiy <adriandecita@gmail.com>
2018-10-04 22:51:14 +0300

    Complete boilerplate setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 import Header from "./components/Header";
 import Films from "./components/Films";
 
--- src/components/Films.js
+++ src/components/Films.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieBlock from "./MovieBlock";
+import './Films.css';
 
 class Films extends Component {
     render() {
